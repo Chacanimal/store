ea539324fe70fd87c86ef5f89a0df6d29b51749c
Chacanimal <migueljesus.sequeira@gmail.com>
2025-04-06 17:39:03 +0100

    mudei so no index nada de especial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from '../../screens/HomeScreen';
 export default HomeScreen;
 
 
 
 
+
+export const options = {
+  tabBarLabel: 'Início',
+  title: 'Página Inicial',
+
+};
